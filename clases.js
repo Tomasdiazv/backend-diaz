5a455ebb5014b31844b0ddf4ae406a1e6f2d949d
Tomas Diaz <tomas_diaz@live.cl>
2022-07-01 00:11:14 -0400

    first commit

---
+++ clases.js
@@ -0,0 +1,37 @@
+class Usuario {
+    constructor(nombre, apellido, libros, mascotas) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+    }
+
+    getFullName() {
+        console.log(`Mi nombre es ${this.nombre} ${this.apellido}`)
+    }
+
+    addBook() {
+        const e = {"libro":this.libros,"auor":"pixula"};
+        return e;
+    }
+
+    getBookName() {
+       const r =  p.addBook();
+       console.log("HOLA HOLA "+p.addBook());
+    }
+
+    addMascota(a) {
+        const array = ['Gato'];
+        array.push(a);
+        console.log(`${this.nombre} tiene ${array.length} mascotas`);
+    }
+}
+
+
+
+const p = new Usuario("Juan", "Perez", "Harry Potter", "Perro");
+console.log(p);
+p.addMascota(p.mascotas)
+p.getFullName();
+p.addBook();
+p.getBookName();
